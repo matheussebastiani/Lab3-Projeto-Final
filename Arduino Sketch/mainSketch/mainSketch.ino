c9c3d5feb836f017757b80273557426e5915bad6
Matheus <matheus.nmt@gmail.com>
2024-11-20 23:40:24 -0300

    Fiz as funções para conseguir os dados dos sensores, não testei e fiquei com medo de configurar o sensor de poeira

--- Arduino Sketch/mainSketch/mainSketch.ino
+++ Arduino Sketch/mainSketch/mainSketch.ino
@@ -25,11 +25,105 @@ int getAirQuality(int pino){
 
 */
 
+int pinoSensorChuva = A1;
+int valorChuva = 0;
+
+int getValorChuva(int pino){ // Sei que essa função é desnecessária, mas acho interessante deixar o loop principal mais limpo, deixando o código mais modularizado e setorizado
+
+  valorChuva = analogRead(pino);
+  return valorChuva;
+
+}
+
+/*
+
+    Definições Sensor LM35
+
+*/
+
+int pinoLM35 = A2;
+float temperatura = 0;
+
+float TemperaturaMedia(int pino){ // Esse sensor é um sensor analógico. como suas medições variam bastante, vou fazer uma média de vários valores lidos, para a temperatura não ficar variando tanto
+
+  float temperatura = 0;
+
+  int n = 10;
+  
+  while(n--){
+
+    tempreatura += analogRead(pino) * ((5.0/1023.0) * 100.0); // Converte valores de tensão para graus celsius
+
+    delay(100);
+
+  }
+
+  return temperatura / 10.0; // Retorna uma média de 10 amostras
+
+}
+
+
+/*
+
+    Definições LDR
+
+*/
+
+int pinoLDR = A3;
+int valorLuminosidade = 0;
+
+int getLuminosidade(int pino){ // também analógico (na verdade um resistor) mas mais facilmente filtrável
+  
+  valorLuminosidade = analogRead(pino);
+
+  return valorLuminosidade;
+
+}
+
+
+/*
+
+    Definições Sensor UV
+    Lembrete: não esquecer de tratar as escalas UV baseados nos níveis de tensão do sensor
+    A tabela tá no sketch do sensor
+
+*/
+
+int pinoUV = A4;
+float tensaoSaida;
+float valorSensorUV;
+
+float getTensaoUV(int pino){
+
+  valorSensorUV = analogRead(pino);
+  TensaoSaida = valorSensorUV / (1024.0*5.0) * 1000;   // Convertendo o valor para mili Volts. A saída do sensor varia entre 0 e 1 V;
+  return TensaoSaida;
+
+
+}
+
+float getValorUV(int pino){
+  valorSensorUV = analogRead(pino);
+  return valorSensorUV;
+}
+
+/*
+
+    Definições Sensor de Poeira
+    não vou continuar agora pq to com medinho
+
+*/
+
+
 
 
 void setup() {
   Serial.begin(9600);
   pinMode(pinoSensorMQ2, INPUT);
+  pinMode(pinoSensorChuva, INPUT);
+  pinMode(pinoLM35, INPUT);
+  pinMode(pinoLDR, INPUT);
+  pinMode(pinoUV, INPUT);
 
 }
 
